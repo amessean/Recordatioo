adad41df581e88052455880425703fafdb99040a
Arthur Messean <arthur.messean@gmail.com>
2020-10-03 14:24:56 +0200

    home page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :bookings
+  # get 'bookings/:id/show_photo' => 'bookings#show_photo', as: 'show_photo'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
