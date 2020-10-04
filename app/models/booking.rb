621135a34409eaf3c6309fdd193e491fd68e67d1
Arthur Messean <arthur.messean@gmail.com>
2020-10-04 18:40:03 +0200

    img by default added

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
     belongs_to :user, optional: true
     has_many :reviews, dependent: :destroy
-    CATEGORY = ["Diner", "Party", "Weekend", "Holiday"]
+    CATEGORY = ["Dinner", "Party", "Weekend", "Holidays"]
     has_many :photos, dependent: :destroy
     validates :start_time, :end_time, presence: true
 end
